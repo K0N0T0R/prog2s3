737c47c2e839ea967fdb268ab24fafd2eb97a93d
Christoph Pletz <chrpletz14@gmail.com>
2022-12-05 14:20:39 +0100

    bruh

--- header.h
+++ header.h
@@ -1,4 +1,6 @@
-using namespace std
+#include <iostream>
+#include <vector>
+using namespace std;
 
 class buchung{
 private:
@@ -9,12 +11,22 @@ public:
 void ausgabe();
     buchung(/* args */);
     ~buchung();
+    void setnummer();
+    void setBetrag();
+    void setDatum();
+    unsigned int getbuchungsnummer();
+    double getBetrag();
+
 };
 
 class girokonto{
 private:
     unsigned int kontonummer;
-
+    vektor<*buchung> buchungen;
+    unsigned int setnummer(unsigned int in);
+    void storno();
+    void saldo();
+    void ausgabeBuchungen(); 
 public:
     girokonto(/* args */);
     ~girokonto();
