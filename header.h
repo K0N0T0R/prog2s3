6efcf7e544a3016555cf6308fe85a8ec4b3b69ee
Christoph Pletz <chrpletz14@gmail.com>
2022-12-05 14:20:27 +0100

    funktionsprototypen

---
+++ header.h
@@ -0,0 +1,37 @@
+using namespace std
+
+class buchung{
+private:
+    unsigned int buchungsnummer;
+    double Betrag;
+
+public:
+void ausgabe();
+    buchung(/* args */);
+    ~buchung();
+};
+
+class girokonto{
+private:
+    unsigned int kontonummer;
+
+public:
+    girokonto(/* args */);
+    ~girokonto();
+};
+
+girokonto::girokonto(/* args */)
+{
+}
+
+girokonto::~girokonto()
+{
+}
+
+buchung::buchung(/* args */)
+{
+}
+
+buchung::~buchung()
+{
+}
---
+++ main.cpp
@@ -0,0 +1,4 @@
+int main(void){
+    using namespace std;
+    return 0;
+}
